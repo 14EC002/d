25d922c33f87be2a43411703d09bdb81a30d59ae
14EC002 <govindhpavi@gmail.com>
2017-06-29 11:53:33 +0530

    Create asci.java

---
+++ asci.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+import java.util.*;
+
+class asci{
+public static void main(String[] args){
+for(int i=0;i<=26;i++)
+{
+System.out.println('a'+i);
+
+System.out.println('A'+i);
+
+}
+}
+}
